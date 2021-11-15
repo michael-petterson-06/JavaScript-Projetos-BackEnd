211e4ca6367c9f945309672636c966e677e9d028
Michael <michaelpetterson06.com.br>
2021-11-15 17:10:54 -0300

    test

---
+++ 25-store-manager/middlewares/handleError.js
@@ -0,0 +1,9 @@
+module.exports = (err, _req, res, _next) => {
+  const NOT_FOUND = 404;
+
+  // console.log(err);
+
+  if (err.err) return res.status(err.err.data.errType).json(err);
+
+  return res.status(NOT_FOUND).json(err);
+};
